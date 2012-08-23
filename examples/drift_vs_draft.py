a59c6454d3e2d4cf838f9ffd18e6a35052b1db5e
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2012-08-23 19:04:11 +0200

    new example included

--- examples/drift_vs_draft.py
+++ examples/drift_vs_draft.py
@@ -1,40 +1,53 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import FFPopSim as h
 
 L=256
 
 ### set up
-pop = h.haploid_highd(L)             #produce an instance of haploid_lowd with L loci
-pop.carrying_capacity = 10000
-pop.outcrossing_rate = 0.1
-pop.crossover_rate = 1.0/pop.L
-pop.mutation_rate = 0.4/pop.carrying_capacity
+pop = h.haploid_highd(L)                        #produce an instance of haploid_lowd with L loci
+pop.carrying_capacity = 50000                   #set the average population size to 50000
+pop.outcrossing_rate = 1                        #make the species obligate outcrossing
+pop.crossover_rate = 0.02/pop.L                 #set the crossover rate of the segment to 2 centimorgans
+pop.mutation_rate = 0.1/pop.carrying_capacity   #per locus mutation rate equal to 0.1/N
 
-selection_coefficients = 0.0*np.ones(pop.L)
-selection_coefficients[::10] = -0.1
+m=10
+selection_coefficients = 0.0*np.ones(pop.L)     #most loci are neutral
+selection_coefficients[::m] = -0.1              #every m-th locus is strongly deleterious
 
-initial_allele_frequencies = 0.5*np.ones(pop.L)
-initial_allele_frequencies[::10] = 0
+initial_allele_frequencies = 0.5*np.ones(pop.L) #define some initial allele frequencies
+initial_allele_frequencies[::m] = 0
 
+#initialize the population in LD with the specified allele frequencies
 pop.set_allele_frequencies(initial_allele_frequencies, pop.carrying_capacity)
 pop.set_additive_trait(selection_coefficients)
 
 
-maxgen = 1000
+#evolve for 2000 generations and track the allele frequencies
+maxgen = 2000
 allele_frequencies = [pop.get_allele_frequencies()]
 tp = [pop.generation]
 while pop.generation<maxgen:
-    pop.evolve(10)
-    print pop.N
-    if (pop.generation%50==0):
-        selection_coefficients[10*np.random.randint(0,25)] = 0.01
-        pop.set_additive_trait(selection_coefficients)
-    allele_frequencies.append(pop.get_allele_frequencies())
-    tp.append(pop.generation)
+    pop.evolve(10)                              #procede 10 generations
+    if (pop.generation%200==0):                 #every 200 generations, make one of the deleterious mutations beneficial
+        print "generation:", pop.generation, 'out of', maxgen
+        selection_coefficients[m*np.random.randint(0,25)] = 0.01
+        pop.set_additive_trait(selection_coefficients)      #update fitness function
 
+    allele_frequencies.append(pop.get_allele_frequencies()) #save the allele frequencies
+    tp.append(pop.generation)                               #and the associated generation
 
+#convert to an array to enable slicing
 allele_frequencies=np.array(allele_frequencies)
 
-plt.plot(tp, allele_frequencies)
+plt.figure()
+for locus in xrange(0,pop.L,m):         #plot the allele frequency trajectories of the selected mutations
+    plt.plot(tp, allele_frequencies[:,locus], c=cm.cool(locus),lw=2)
 
+for locus in xrange(5,pop.L,50):        #plot a few neutral trajectories
+    plt.plot(tp, allele_frequencies[:,locus], c=cm.cool(locus), lw=2)
+
+plt.title('Drift and Draft')
+plt.xlabel('Time [generations]')
+plt.ylabel('Allele frequencies')
