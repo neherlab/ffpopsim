1bacae0200123c60d2ad60f261c68f13baaa838d
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2012-09-06 12:30:33 +0200

    License moved to GPL because of GSL (GPL is viral)

--- src/python/FFPopSim.py
+++ src/python/FFPopSim.py
@@ -116,6 +116,8 @@ def _swig_setattr_nondynamic_method(set):
     return set_attr
 
 
+LICENSE = '''FFPopSim is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. FFPopSim is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with FFPopSim. If not, see <http://www.gnu.org/licenses/>.'''
+
 FREE_RECOMBINATION = _FFPopSim.FREE_RECOMBINATION
 CROSSOVERS = _FFPopSim.CROSSOVERS
 SINGLE_CROSSOVER = _FFPopSim.SINGLE_CROSSOVER
