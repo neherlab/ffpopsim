09c86367a282436d11e2654067c90af71285548c
Fabio Zanini <fabio.zanini@tuebingen.mpg.de>
2012-09-13 10:17:27 +0200

    More doc issues with C++

--- doc/cpp/mainpage.cpp
+++ doc/cpp/mainpage.cpp
@@ -28,6 +28,22 @@
  * Examples and tests are found in the `tests` folder. They include a few functions that show that the Fast Fourier Transform-based algorithm for
  * recombination yields the same result as the more expensive naive algorithm.
  *
+ * @section compiling Compiling and Linking against FFPopSim
+ *
+ * To build the C++ part of FFPopSim, refer to the INSTALL file.
+ * Building your own programs using FFPopSim is then a two-step process, @em compiling @em and @em linking @em.
+ * For the compile step, you need to import the header(s) you are using, like in the example above in this page. The FFPopSim headers include recursively all sublibraries such as GSL. Moreover, the compiler must be told where those headers are located; a typical compile call using g++ is the following:
+ * \verbatim
+ g++ -c -I../pkg/include/ -o example_lowd.o example_lowd.cpp
+ \endverbatim
+ *
+ * For the link step, the following libraries must be used: FFPopSim, gsl, gslcblas. The linker must be told where to find them. Since FFPopSim is compiled statically, its archive must be included in the list of target files. A typical link call using g++ is the following:
+ * \verbatim
+ g++ example_lowd.o ../pkg/lib/libFFPopSim.a -L../pkg/lib/ -lFFPopSim -lgsl -lgslcblas -o example_lowd
+ \endverbatim
+ *
+ * Of course, compile and link options such as optimization levels can be normally used with FFPopSim.
+ *
  * @section python Python Interface
  *
  * FFPopSim also includes bindings to Python 2.7. If you are starting using the library and have Python and all other requirements installed, it is
