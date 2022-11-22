13f2491f12ce90bc921585c420db4edcf7c07ce9
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-11-23 00:12:14 +0100

    fix: correct type of the input in "mutation_selection" test
    Integer is expected:
    ```
      Traceback (most recent call last):
        File "/project/examples/mutation_selection_balance_lowd.py", line 46, in <module>
          pop.evolve(0.1 * N)             # N / 10 generations between successive samples
          ^^^^^^^^^^^^^^^^^^^
        File "/tmp/tmp.dpQseITW26/venv/lib/python3.11/site-packages/FFPopSim.py", line 751, in evolve
          val = _FFPopSim.haploid_lowd_evolve(self, *args, **kwargs)
                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      TypeError: in method 'haploid_lowd_evolve', argument 2 of type 'int'
    ```

--- examples/mutation_selection_balance_lowd.py
+++ examples/mutation_selection_balance_lowd.py
@@ -1,7 +1,7 @@
 '''
 author:     Richard Neher, Fabio Zanini
 date:       11/07/12
-content:    Example on the steady state distribution of allele frequency in a 
+content:    Example on the steady state distribution of allele frequency in a
             balance between mutation and genetic drift using haploid_lowd.
 '''
 # Import modules (setting the path should not be necessary when the module is
@@ -19,14 +19,14 @@ N = 500                             # population size
 L = 4                               # number of loci
 s = np.linspace(-0.2 ,0.7, L) / N   # additive selection coefficients for L loci, scaled to N
 mu = 0.4 / N                        # mutation rate, scaled to N
-r = 5.0 / N                         # recombination rate for each interval between loci. 
+r = 5.0 / N                         # recombination rate for each interval between loci.
 
 # set up population
 pop = h.haploid_lowd(L)             # produce an instance of haploid_lowd with L loci
 
 # set and additive fitness function. Note that FFPopSim models fitness landscape
 # in a +/- rather than 0/1 basis, hence the factor 1/2
-pop.set_fitness_additive(0.5 * s) 
+pop.set_fitness_additive(0.5 * s)
 
 pop.set_mutation_rates(mu)          # mutation rate
 pop.set_recombination_rates(r)      # recombination rate (CROSSOVERS model by default)
@@ -40,10 +40,10 @@ pop.status()
 pop.evolve(10 * N)                  # run for 10N generations to equilibrate
 
 # evolve and sample allele frequencies
-nsamples = 10000      
+nsamples = 10000
 allele_frequencies = np.zeros((nsamples,L))
 for ii in range(nsamples):
-    pop.evolve(0.1 * N)             # N / 10 generations between successive samples
+    pop.evolve(N // 10)             # N / 10 generations between successive samples
 
     # get allele frequencies
     allele_frequencies[ii,:] = pop.get_allele_frequencies()
@@ -63,7 +63,7 @@ for locus in range(L):
 
     # plot
     plt.plot(bin_centers, y, color=cols[locus], label = r'$N s_'+str(locus+1)+'='+str(N*s[locus])+'$')
-    
+
     # calculate the diffusion theory single locus result
     diffusion_theory = nu**(2*N*mu-1)*(1-nu)**(2*N*mu-1)*np.exp(2*N*s[locus]*nu)
     # normalize
